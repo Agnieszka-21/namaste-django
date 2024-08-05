87d0fb71d78d6ed2d534260547d59a6754e84e0b
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-08-05 16:31:08 +0000

    Update the profile.html template and its styling

--- user_profiles/urls.py
+++ user_profiles/urls.py
@@ -3,6 +3,7 @@ from .views import profile, home, error404
 
 urlpatterns = [
     path('', home, name='home'),
-    path('profile/<str:pk>/', profile, name='profile'),
+    path('profile/', profile, name='profile'),
+    #path('profile/<str:pk>/', profile, name='profile'),
     path('404/', error404, name='error404'),
 ]
