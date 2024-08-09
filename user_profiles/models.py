3bcd70163034119dcadafb67593a80a6783f5f3c
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-08-09 16:58:52 +0000

    Add Django Signals to automatically create a profile when a user signs up

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -14,13 +14,24 @@ placeholder_image = '../static/images/default_profile_pic.jpg'
 
 class Profile(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)  # https://www.geeksforgeeks.org/uuidfield-django-models/
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     date_of_birth = models.DateField(null=True, blank=True)
-    injuries = models.TextField(max_length=300, blank=True)
+    injuries = models.TextField(max_length=300, null=True, blank=True)
     profile_pic = CloudinaryField('image', default=placeholder_image, blank=True)
-    signed_waiver = models.BooleanField()
+    signed_waiver = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the profile
+    """
+    if created:
+        Profile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.profile.save()
 
 
