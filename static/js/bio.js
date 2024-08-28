7f7fe19476db76203fd32e2c6c4d977fc24df7fd
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-08-28 07:40:17 +0000

    Trap focus in the bio modal

--- static/js/bio.js
+++ static/js/bio.js
@@ -8,8 +8,13 @@ const spanCloseModalBio = document.getElementById('close-bio');
 // Open the bio modal when the user activates the question-mark button (click or Enter/space key)
 function displayModalBio() {
     modalBio.classList.remove('hide');
+    console.log('Class hide removed');
     modalBio.classList.add('show');
+    console.log('Class show added');
     modalBio.focus();
+    console.log('Right after modalBio.focus function');
+    trapFocus();
+    console.log('Right after trapFpcus function');
 }
 
 bioTrigger.addEventListener('click', displayModalBio);
@@ -38,3 +43,32 @@ window.addEventListener('click', function (e) {
         modalBio.classList.add('hide');
     }
 });
+
+
+//https://uxdesign.cc/how-to-trap-focus-inside-modal-to-make-it-ada-compliant-6a50f9a70700
+//https://vaskort.com/a-trap-focus-function-you-need-for-your-modals/const trapFocus = ((element, prevFocusableElement = document.activeElement) => {
+
+function trapFocus() {
+    const focusableElBio = modalBio.querySelector('#close-asmr');
+    const KEYCODE_TAB = 9;
+
+    modalBio.addEventListener('keydown', function (e) {
+        let isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+        if (!isTabPressed) {
+            return;
+        }
+
+        if (e.shiftKey) /* shift + tab */ {
+            if (document.activeElement === modalBio) {
+                focusableElBio.focus();
+                e.preventDefault();
+            }
+        } else /* tab */ {
+            if (document.activeElement === focusableElBio) {
+                modalBio.focus();
+                e.preventDefault();
+            }
+        }
+    });
+}
