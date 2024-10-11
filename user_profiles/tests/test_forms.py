5efa91ff70d127cf3c7f5be508c7e1db9d5f4daf
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-10-11 20:00:45 +0000

    Add further automated tests for forms in the User Profiles app

--- user_profiles/tests/test_forms.py
+++ user_profiles/tests/test_forms.py
@@ -32,8 +32,30 @@ class CustomSignupFormTest(SimpleTestCase):
         form = CustomSignupForm()
         self.assertTrue(form.fields['last_name'].label == 'Last Name')
 
+    def test_first_name_is_required(self):
+        """
+        Tests whether the field 'first_name' is required
+        """
+        form = CustomSignupForm()
+        self.assertTrue(form.fields['first_name'].required)
+
+    def test_last_name_is_required(self):
+        """
+        Tests whether the field 'first_name' is required
+        """
+        form = CustomSignupForm()
+        self.assertTrue(form.fields['last_name'].required)
+
 
 class ClientProfileFormTest(SimpleTestCase):
+
+    def test_dob_field_label(self):
+        """
+        Tests the label of the field 'date_of_birth'
+        """
+        form = ClientProfileForm()
+        self.assertTrue(
+            form.fields['date_of_birth'].label == 'Date of birth')
 
     def test_injuries_field_label(self):
         """
@@ -49,3 +71,37 @@ class ClientProfileFormTest(SimpleTestCase):
         """
         form = ClientProfileForm()
         self.assertTrue(form.fields['profile_pic'].label == 'Profile image')
+
+    def test_dob_is_not_required(self):
+        """
+        Tests whether the field 'date_of_birth' is not required
+        """
+        form = ClientProfileForm()
+        self.assertFalse(form.fields['date_of_birth'].required)
+
+    def test_injuries_is_not_required(self):
+        """
+        Tests whether the field 'injuries' is not required
+        """
+        form = ClientProfileForm()
+        self.assertFalse(form.fields['injuries'].required)
+
+    def test_profile_pic_is_not_required(self):
+        """
+        Tests whether the field 'profile_pic' is not required
+        """
+        form = ClientProfileForm()
+        self.assertFalse(form.fields['profile_pic'].required)
+
+    def test_dob_placeholder(self):
+        """
+        Test input type and placeholder value for the
+        'date_of_birth' field
+        """
+        form = ClientProfileForm()
+        self.assertEqual(
+            form.fields['date_of_birth'].widget.__class__.__name__,
+            'DateInput')
+        self.assertEqual(
+            form.fields['date_of_birth'].widget.attrs['placeholder'],
+            'YYYY-MM-DD')
